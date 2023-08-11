c07a29d8503034ae5d407bb487a564d7e077439b
Bertil Chapuis <bchapuis@gmail.com>
2023-08-11 14:05:27 +0200

    Some simplifications and cleaning

---
+++ src/main/java/org/acme/LocationEntity.java
@@ -0,0 +1,181 @@
+package org.acme;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity(name = "location")
+public class LocationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private double longitude;
+    private double latitude;
+    private double altitude;
+    private double accuracy;
+    private double altitudeAccuracy;
+    private double heading;
+    private double speed;
+    private String ip;
+    private String headers;
+    private Date createdAt;
+    private String macAddress;
+    private String acquisitionMode;
+    private String cookie;
+
+    public LocationEntity() {
+    }
+
+    public LocationEntity(double longitude, double latitude, double altitude, double accuracy, double altitudeAccuracy, double heading, double speed, String ip, String headers, Date createdAt, String macAddress, String acquisitionMode, String cookie) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.altitude = altitude;
+        this.accuracy = accuracy;
+        this.altitudeAccuracy = altitudeAccuracy;
+        this.heading = heading;
+        this.speed = speed;
+        this.ip = ip;
+        this.headers = headers;
+        this.createdAt = createdAt;
+        this.macAddress = macAddress;
+        this.acquisitionMode = acquisitionMode;
+        this.cookie = cookie;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(double altitude) {
+        this.altitude = altitude;
+    }
+
+    public double getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(double accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public double getAltitudeAccuracy() {
+        return altitudeAccuracy;
+    }
+
+    public void setAltitudeAccuracy(double altitudeAccuracy) {
+        this.altitudeAccuracy = altitudeAccuracy;
+    }
+
+    public double getHeading() {
+        return heading;
+    }
+
+    public void setHeading(double heading) {
+        this.heading = heading;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(String headers) {
+        this.headers = headers;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
+
+    public String getAcquisitionMode() {
+        return acquisitionMode;
+    }
+
+    public void setAcquisitionMode(String acquisitionMode) {
+        this.acquisitionMode = acquisitionMode;
+    }
+
+    public String getCookie() {
+        return cookie;
+    }
+
+    public void setCookie(String cookie) {
+        this.cookie = cookie;
+    }
+
+    @Override
+    public String toString() {
+        return "UserData{" +
+                "id=" + id +
+                ", longitude=" + longitude +
+                ", latitude=" + latitude +
+                ", altitude=" + altitude +
+                ", accuracy=" + accuracy +
+                ", altitudeAccuracy=" + altitudeAccuracy +
+                ", heading=" + heading +
+                ", speed=" + speed +
+                ", ip='" + ip + '\'' +
+                ", headers='" + headers + '\'' +
+                ", createdAt=" + createdAt +
+                ", macAddress='" + macAddress + '\'' +
+                ", acquisitionMode='" + acquisitionMode + '\'' +
+                ", cookie='" + cookie + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/acme/LocationResource.java
@@ -0,0 +1,37 @@
+package org.acme;
+
+import io.vertx.core.http.HttpServerRequest;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+
+@Singleton
+@Path("/")
+public class LocationResource {
+
+    @Inject
+    EntityManager em;
+
+    @POST
+    @Path("/location")
+    @Transactional
+    public Response collectiploc(
+            @Context HttpServerRequest request,
+            @FormParam("longitude") double longitude,
+            @FormParam("latitude") double latitude,
+            @FormParam("altitude") double altitude,
+            @FormParam("accuracy") double accuracy,
+            @FormParam("altitudeAccuracy") double altitudeAccuracy,
+            @FormParam("heading") double heading,
+            @FormParam("speed") double speed) {
+        var headers = request.headers();
+        var address = request.connection().remoteAddress().hostAddress();
+        var userData = new LocationEntity(longitude, latitude, altitude, accuracy, altitudeAccuracy, heading, speed, address, headers.toString(), new java.util.Date(), "macAddress", "acquisitionMode", "cookie");
+        em.persist(userData);
+        return Response.ok().build();
+    }
+}
